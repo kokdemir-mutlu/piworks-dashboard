62ea93998348c005fd1512f63bb30c35109da357
Mutlu Kokdemir <kokdemir.mutlu@gmail.com>
2021-09-01 23:54:46 +0300

    Added some part of initial template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DxResponsiveBoxModule } from 'devextreme-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DxResponsiveBoxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
